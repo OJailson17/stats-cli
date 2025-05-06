79aa0796a1b0d95bb3c3d846f894b398b15376f6
OJailson17 <JaylsonO17@gmail.com>
2025-05-06 02:37:03 -0300

    fix: accounts not being reset

--- src/commands/reset/accounts/actions/reset-accounts.ts
+++ src/commands/reset/accounts/actions/reset-accounts.ts
@@ -1,16 +1,14 @@
-import { createAccounts } from 'commands/create/accounts/actions/create-accounts';
 import { pg } from 'lib/postgres';
 
 export const resetAccounts = async () => {
 	console.log('Resetting Accounts...');
 	try {
-		const query = `
-    TRUNCATE TABLE stats_finances_bank_accounts CASCADE;
-    `;
+		await pg.connect();
+
+		const query = `TRUNCATE stats_finances_bank_accounts CASCADE;`;
 
 		await pg.query(query);
 
-		await createAccounts();
 		console.log('Accounts reset!');
 	} catch (error) {
 		console.log(error);
