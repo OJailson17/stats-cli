67a62bd5e42467ca6bdc148eb8ba04199501daa1
OJailson17 <JaylsonO17@gmail.com>
2025-09-20 23:02:22 -0300

    chore: format accounts output

--- src/commands/list/accounts/action/list-accounts.ts
+++ src/commands/list/accounts/action/list-accounts.ts
@@ -1,14 +1,32 @@
 import { pg } from 'lib/postgres';
 
+type QueryResponse = {
+	account: string;
+	balance: string;
+};
+
 export const listAccounts = async () => {
 	try {
 		await pg.connect();
 
-		const { rows: accounts } = await pg.query(
+		const { rows } = await pg.query(
 			'SELECT account, balance FROM stats_finances_bank_accounts;',
 		);
 
-		console.log({ accounts });
+		const accounts = rows as QueryResponse[];
+		accounts.sort((acc1, acc2) => Number(acc2.balance) - Number(acc1.balance));
+
+		const formattedAccounts = accounts.map(account => {
+			return {
+				account: account.account,
+				balance: new Intl.NumberFormat('pt-BR', {
+					style: 'currency',
+					currency: 'BRL',
+				}).format(Number(account.balance)),
+			};
+		});
+
+		console.table(formattedAccounts);
 	} catch (error) {
 		console.log(error);
 		await pg.end();
